f4381fe114befc6bd40c69337077c414af651b1b
Alecoder73 <alejandragalvanbojorquez@gmail.com>
2024-11-23 11:21:03 -0600

    Modifique el header y el formulario para crear cuenta

--- Public/js/scripts.js
+++ Public/js/scripts.js
@@ -1,18 +1,39 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    const signupSection = document.getElementById('signup-section');
-    const loginSection = document.getElementById('login-section');
-    const signupButton = document.querySelector('.sign-up button');
-    const loginButton = document.querySelector('.login-btn');
-
-    signupButton.addEventListener('click', function () {
-        signupSection.style.display = 'none';
-        loginSection.style.display = 'block'; 
+document.addEventListener("DOMContentLoaded", () => {
+    // Select necessary elements
+    const formsContainer = document.querySelector(".forms");
+    const loginForm = document.querySelector(".form.login");
+    const signupForm = document.querySelector(".form.signup");
+    const signupLink = document.querySelector(".signup-link");
+    const loginLink = document.querySelector(".login-link");
+    const pwShowHide = document.querySelectorAll(".eye-icon");
+  
+    // Toggle between Login and Signup forms
+    signupLink.addEventListener("click", (e) => {
+      e.preventDefault();
+      formsContainer.classList.add("show-signup");
+      loginForm.style.display = "none"; // Hide login form
+      signupForm.style.display = "block"; // Show signup form
     });
-
-    loginButton.addEventListener('click', function () {
-        loginSection.style.display = 'none';
-        signupSection.style.display = 'block'; 
+  
+    loginLink.addEventListener("click", (e) => {
+      e.preventDefault();
+      formsContainer.classList.remove("show-signup");
+      signupForm.style.display = "none"; // Hide signup form
+      loginForm.style.display = "block"; // Show login form
     });
-
-});
+  
+    // Show/hide password functionality
+    pwShowHide.forEach((eyeIcon) => {
+      eyeIcon.addEventListener("click", () => {
+        const pwField = eyeIcon.previousElementSibling;
+        if (pwField.type === "password") {
+          pwField.type = "text";
+          eyeIcon.classList.replace("bx-hide", "bx-show");
+        } else {
+          pwField.type = "password";
+          eyeIcon.classList.replace("bx-show", "bx-hide");
+        }
+      });
+    });
+  });
+  
